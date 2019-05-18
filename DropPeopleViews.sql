df0d145c0ec1e11d73d6c6580a688865f8d48519
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-18 16:35:26 +0100

    Added drop views script

---
+++ DropPeopleViews.sql
@@ -0,0 +1,9 @@
+drop view PMPoints;
+drop view DPMPoints;
+drop view LOPoints;
+drop view DLOPoints;
+drop view MGPoints;
+drop view GWPoints;
+drop view MOPoints;
+drop view OWPoints;
+drop view PeoplePoints;
