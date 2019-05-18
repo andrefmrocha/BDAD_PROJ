e761c21e78fa4408093af29adc03f2423c6c0117
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-18 18:21:30 +0100

    Added 3rd trigger

---
+++ T3.sql
@@ -0,0 +1,19 @@
+create Trigger T3
+Before Insert on Specification
+For Each Row
+When not exists(
+select *
+from Round, TournamentDebate
+where Round.id = TournamentDebate.round and Round.name = 'SemiFinal' and TournamentDebate.id = New.debate
+)
+or New.team not in
+(
+select Team.id
+from Tournament, Team, TournamentTeam
+where Tournament.id =  TournamentTeam.tournament and Team.id = TournamentTeam.team
+order by Team.points desc
+LIMIT 8
+)
+Begin
+Select raise(ignore);
+End
