457f45af69583d8502380ed0f539e9117f23e46c
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-22 22:10:18 +0100

    Updated query 1

--- Queries/int1.sql
+++ Queries/int1.sql
@@ -1,5 +1,6 @@
 select motion, sum(speakerPointsMean) as speakerPointsSum
 from Round JOIN TournamentDebate
-where Round.id = TournamentDebate.round
+where Round.id = TournamentDebate.round and 
+(Round.name = 'R1' or Round.name = 'R2' or Round.name = 'R3' or Round.name = 'R4' or Round.name = 'R5' )
 group by Round.motion
 order by speakerPointsSum desc
