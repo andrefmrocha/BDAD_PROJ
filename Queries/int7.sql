20c7345a54a56add7423a91e2e2ed18441112418
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-22 21:59:44 +0100

    Progress on 7

--- Queries/int7.sql
+++ Queries/int7.sql
@@ -1,9 +1,37 @@
-select Person.id, Adjudicator.debate, Frequent.university as AjudicatorUni, Specification.classification, Specification.position, Specification.firstSpeaker, Specification.secondSpeaker
-from Adjudicator, (Person Natural Join Student), Frequent , (Debate Natural Join TournamentDebate), Specification,  PeoplePoints
-where Adjudicator.person = 13 and Person.id = Frequent.person and Adjudicator.person = Person.id and Adjudicator.debate = Debate.id
-and Specification.debate = Debate.id 
-and exists 
-(select * 
-from University 
-where University.id = Frequent.university and AjudicatorUni = Frequent.person and
-(Specification.firstSpeaker = PeoplePoints.id or Specification.firstSpeaker = PeoplePoints.id))
+select
+  Person.id,
+  Adjudicator.debate,
+  Frequent.university as AjudicatorUni
+from
+  Adjudicator,
+  (
+    Person Natural
+    Join Student
+  ),
+  Frequent,
+  (
+    Debate Natural
+    Join TournamentDebate
+  ) as AjudicatorDebate,
+  PeoplePoints
+where
+  Adjudicator.person = 13
+  and Person.id = Frequent.person
+  and Adjudicator.person = Person.id
+  and Adjudicator.debate = Debate.id
+  and exists (
+    select
+      *
+    from
+      University,
+      TournamentDebate,
+      Specification
+    where
+      University.id = AjudicatorUni
+      and TournamentDebate.id = AjudicatorDebate.id
+      and Specification.debate = Debate.id
+      and (
+        Specification.firstSpeaker = PeoplePoints.id
+        or Specification.secondSpeaker = PeoplePoints.id
+      )
+  )
