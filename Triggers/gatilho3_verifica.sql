e41a64beb7bac995cb60c54b846dce7d954e1905
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-25 12:46:10 +0100

    Added specification to trigger 3

---
+++ Triggers/gatilho3_verifica.sql
@@ -0,0 +1,7 @@
+select *
+from Specification, TournamentDebate
+where Specification.debate = 49 and Specification.debate = TournamentDebate.id 
+INSERT INTO Specification (position,classification,firstSpeaker,secondSpeaker,team,debate) VALUES ("CO","4",64,65,47,"49");
+select *
+from Specification, TournamentDebate
+where Specification.debate = 49 and Specification.debate = TournamentDebate.id 
